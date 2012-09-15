d807787875ac24a951a16244ab38d5a7a2a89bae
Josef Cech <czech.josef@gmail.com>
2012-09-15 18:26:25 +0200

    macros.rb: backlinks macro

--- lib/redmine/wiki_formatting/macros.rb
+++ lib/redmine/wiki_formatting/macros.rb
@@ -86,6 +86,39 @@ module Redmine
         content_tag('dl', out)
       end
 
+      desc "Shows backlinks to the/a wiki page. Example:\n\n  !{{backlinks}}"
+      macro :backlinks do |obj, args|
+        if obj.is_a?(WikiContent) || obj.is_a?(WikiContent::Version)
+          page = obj.page
+        else
+          raise 'With no argument, this macro can be called from wiki pages only.'
+        end
+        raise 'Page not found' if page.nil? || !User.current.allowed_to?(:view_wiki_pages, page.wiki.project)
+
+        all_projects_search_begin = "[[" + page.project.identifier + ":" + page.title;
+        results_all_projects, = WikiPage.search(
+          [all_projects_search_begin + "]]", all_projects_search_begin + "|"],
+          nil, # all projects
+          :titles_only => false
+        )
+        results_current_project, = WikiPage.search(
+          ["[[" + page.title + "]]", "[[" + page.title + "|"],
+          @project,
+          :titles_only => false
+        )
+        results = results_all_projects + results_current_project
+
+        links = results.map do |page|
+          title = page.pretty_title
+          title = page.project.name + ": " + title if page.project != @project
+          url_params = { :controller => 'wiki', :action => 'show', :project_id => page.project, :id => page.title }
+          link_to title, url_params
+        end
+
+        content = "Backlinks: " + links.join(" • ")
+        content.html_safe
+      end
+
       desc "Displays a list of child pages. With no argument, it displays the child pages of the current wiki page. Examples:\n\n" +
              "  !{{child_pages}} -- can be used from a wiki page only\n" +
              "  !{{child_pages(Foo)}} -- lists all children of page Foo\n" +
